26e44976fe26fdbc4ece4949c9bd3d0522e799e3
eyad amr <eyadamr1644@gmail.com>
2026-02-28 09:39:58 +0200

    first commit

---
+++ simpleShell.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main() {
+    printf("welcome from main session\n");
+    return 0;
+}
